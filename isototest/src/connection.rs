3d40e13b8ec02fb529be43f1fd7529ea180d5de2
ByteOtter <christopher-hock@protonmail.com>
2024-07-19 20:33:31 +0200

    Add documentation and erro handling to isototest

--- isototest/src/connection.rs
+++ isototest/src/connection.rs
@@ -1,11 +1,42 @@
 use tokio::{self, net::TcpStream};
 use vnc::{PixelFormat, VncClient, VncConnector, VncError};
 
+/// Create a new VNC client.
+///
+/// During the connection process the connection to the VNC server is
+/// tested.
+///
+/// # Parameters
+///
+/// * target_ip: `String` - The IP and port of the VNC target server. (e.g `172.0.0.1:5900`)
+/// * psw: `String` - The password used for authenticating with the server. (If the server
+/// does not use authentication, this is irrelevant.)
+///
+/// # Returns
+///
+/// * vnc: `Ok(VncClient)` - A new instance of a `vnc-rs` `VncClient`.
+/// * `Err(VncError)` - A `VncError` type, depending on the cause of failure.
+///
+/// # Panics
+///
+/// This function may panic, if the connection to the VNC server or the
+/// configuration of the client fails.
 pub async fn create_vnc_client(
     target_ip: String,
-    psw: Option<String>,
+    mut psw: Option<String>,
 ) -> Result<VncClient, VncError> {
-    let tcp: TcpStream = TcpStream::connect(target_ip).await?;
+    if psw.is_none() {
+        psw = Some(String::new());
+    }
+
+    let tcp: TcpStream = match TcpStream::connect(target_ip).await {
+        Ok(tcp) => tcp,
+        Err(e) => {
+            let err = VncError::IoError(e);
+            return Err(err);
+        }
+    };
+
     let vnc: VncClient = VncConnector::new(tcp)
         .set_auth_method(async move { Ok(psw.unwrap()) })
         .add_encoding(vnc::VncEncoding::Tight)
