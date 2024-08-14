50b3f8942fe47ddb4aff5c44377e8cdfefa32557
ByteOtter <christopher.hock@suse.com>
2024-08-14 12:47:10 +0200

    Create isotoenv library

---
+++ isotoenv/src/lib.rs
@@ -0,0 +1,23 @@
+//! Prepare a test environment for [openQA](https://open.qa).
+//!
+//! This crate is responsible for preparing an `openQA` worker machine or VM for executing the test suite.
+//!
+//! ## Optional Features
+//!
+//! * `default-logging` - Provides you with a sensible logger configuration using the `env_logger` crate.
+
+// Organize library structure
+pub mod logging;
+pub mod errors;
+
+// Provide code on the root level of the library
+#[cfg(feature = "default-logging")]
+use crate::logging::init_default_logging;
+#[cfg(feature = "default-logging")]
+use crate::errors::util_errors::LoggingError;
+
+#[cfg(feature = "default-logging")]
+pub fn init_logging(level: Option<&str>) -> Result<(), LoggingError> {
+    init_default_logging(level)?;
+    Ok(())
+}
